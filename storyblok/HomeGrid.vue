d3ae1f77d860e50c244be769f1bf59f1f1b5d455
evertonk <evertonluizcoster@gmail.com>
2025-12-16 21:53:24 -0300

    fix: fixed css of cards on news list

--- storyblok/HomeGrid.vue
+++ storyblok/HomeGrid.vue
@@ -7,7 +7,7 @@ const props = defineProps<{ blok: any }>()
 
 <template>
   <section v-editable="blok" class="h-auto flex items-center justify-center">
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
       <CategoryCards
         v-for="category in blok.categories"
         :key="category._uid"
--- storyblok/NewsCard.vue
+++ storyblok/NewsCard.vue
@@ -2,14 +2,16 @@
   <div
     class="flex flex-row bg-white border border-[#eee] rounded-lg shadow-sm overflow-hidden w-full w-max-[800px] text-center"
   >
-      <div class="news-card-grid grid md:grid-cols-[300px_1fr]">
+      <div class="news-card-grid grid md:grid-cols-[300px_1fr] min-h-[250px]">
         <div class="news-image">
           <img class="object-cover h-full" :src="news.image" :alt="news.title" />
         </div>
 
-        <div class="news-content flex flex-col p-5 items-start bg-white">
-          <h2 class="news-title mb-5 text-2xl font-bold text-[#1a1a1a]">{{ news.title }}</h2>
-          <p class="news-description text-sm text-[#4a4a4a] text-justify mb-5">{{ news.description }}</p>
+        <div class="news-content flex flex-col justify-between p-5 items-start bg-white">
+          <div>
+            <h2 class="news-title mb-5 text-2xl font-bold text-[#1a1a1a] text-left">{{ news.title }}</h2>
+            <p class="news-description text-sm text-[#4a4a4a] text-justify mb-5">{{ news.description }}</p>
+          </div>
 
           <div class="news-footer flex text-xs border-t border-[#eee] pt-2">
             <p class="news-date">{{  formattedDate  }}</p>
