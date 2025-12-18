0f44cdfb6325cafcf37ee374733ac93d06f1983d
evertonk <evertonluizcoster@gmail.com>
2025-12-18 12:18:58 -0300

    fix: fixed back button

--- storyblok/category/CategoryHeader.vue
+++ storyblok/category/CategoryHeader.vue
@@ -7,8 +7,20 @@
 
 <template>
   <header v-editable="blok">
-    <h1 class="text-3xl font-bold items-center w-full h-200px flex justify-center m-6">
-      {{ category?.toLocaleUpperCase() || blok.title }}
-    </h1>
+     <div class="grid grid-cols-1 gap-4 w-full p-6 items-center md:grid-cols-[1fr_9fr]">
+            <div class="flex items-center justify-center">
+              <NuxtLink
+                to="/"
+                class="text-black border-2 border-black text-center rounded-md p-3 max-w-36"
+              >
+                Home
+              </NuxtLink>
+          </div>
+            <div class="flex items-center md:w-[90%] justify-center">
+                 <h1 class="text-3xl font-bold self-center">
+                {{ category?.toLocaleUpperCase() || blok.title }}
+            </h1>
+            </div>
+        </div>
   </header>
 </template>
